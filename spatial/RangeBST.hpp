53523b604492d68281cd032f32ea225b54a82bd0
joseporres <joseporres@users.noreply.github.com>
2021-09-28 22:03:46 -0500

    no c q pasa

--- spatial/RangeBST.hpp
+++ spatial/RangeBST.hpp
@@ -12,17 +12,17 @@ namespace spatial {
 template <typename Point>
 class RangeBST : public SpatialBase<Point>, public BSTree<Point>{
  private:
-  static bool comparePoint(const Point& a, const Point& b){
+  static bool comparePoint( Point& a,  Point& b){
     return a.distance()<b.distance();
   }
  public:
   RangeBST(){};
-  void insert(const Point& new_point) override { this->insertBST(new_point, comparePoint);}
+  void insert( Point& new_point) override { this->insertBST(new_point, comparePoint);}
 
   // El punto de referencia no necesariamente es parte del dataset
-  Point nearest_neighbor(const Point& reference) override { return Point({0}); }
+  Point nearest_neighbor( Point& reference) override { return Point({0}); }
 
-  std::vector<Point> range(const Point& min, const Point& max) override {
+  std::vector<Point> range( Point& min,  Point& max) override {
     return this->rangeSearch(min, max, comparePoint);
   };
 };
